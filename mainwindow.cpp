50b5adb8e3392fe1f9a22f209d92918a9d6fca4f
Valeria Dopira <leradopira@mail.ru>
2018-12-20 12:18:44 +0300

    13th request

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -164,7 +164,7 @@ void MainWindow::setRequests()
     ui->queryComboBox->addItem("Все рейсы, вылетающие из Гамбурга и все рейсы, прилетающие в Доху");
     ui->queryComboBox->addItem("Все самолеты, которые принадлежат авиакомпаниям входящим в альянс OneWorld, в алфавитном порядке по имени авиакомпании");
     ui->queryComboBox->addItem("Все самолеты, которые принадлежат авиакомпаниям вышедшим из альянса OneWorld");
-    ui->queryComboBox->addItem("");
+    ui->queryComboBox->addItem("Все самолеты, прилетающие в Москву и данные о которых есть в таблицах Аэропорты и Самолеты в алфавитном порядке");
     ui->queryComboBox->addItem("");
     ui->queryComboBox->addItem("");
 }
@@ -774,7 +774,63 @@ void MainWindow::on_queryComboBox_activated(int index)
         break;
         }
 
+    case 12:
+        {
+        qDebug() << "Все самолеты, прилетающие в Москву и данные о которых есть в таблицах Аэропорты и Самолеты в алфавитном порядке";
+        QString request = "SELECT TypeOfPlane, Name, Terminal FROM Airports JOIN Airplanes ON (Airplanes.Type = Airports.TypeOfPlane) WHERE Airports.Location = 'Москва' ORDER BY Airports.TypeOfPlane ASC";
+        ui->sqlTextEdit->setText(request);
+        QSqlQuery query = mainWindowController->getSqliteAdapter()->runSQL(request);
 
+        int count = 0;
+        QStringList l1;
+        QStringList l2;
+        QStringList l3;
+        while (query.next())
+        {
+            count++;
+            QString n = query.value(0).toString();
+            l1.push_back(n);
+            QString a = query.value(1).toString();
+            l2.push_back(a);
+            QString d = query.value(2).toString();
+            l3.push_back(d);
+        }
+        qDebug() << count;
+        qDebug() << l1;
+        qDebug() << l2;
+        qDebug() << l3;
+
+        QStringList labels = {
+            "Тип самолета",
+            "Аэропорт",
+            "Терминал"
+        };
+        ui->resultTableWidget->setColumnCount(labels.size());
+        ui->resultTableWidget->setHorizontalHeaderLabels(labels);
+        ui->resultTableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+        ui->resultTableWidget->setRowCount(l1.size());
+
+        for(int i = 0; i < count; i++)
+        {
+            qDebug() << "i: "<< i;
+            for(int j = 0; j < l1.size()/count; j++)
+            {
+//                    qDebug() << "j: "<< j;
+//                    qDebug() << "l.value(i)" << l.value(i);
+//                    qDebug() << "l.value(j)" << l.value(j);
+                QString n = l1.value(i);
+                QTableWidgetItem *item = new QTableWidgetItem(n);
+                QString a = l2.value(i);
+                QTableWidgetItem *itemA = new QTableWidgetItem(a);
+                QString d = l3.value(i);
+                QTableWidgetItem *itemD = new QTableWidgetItem(d);
+                ui->resultTableWidget->setItem(i, j, item);
+                ui->resultTableWidget->setItem(i, j+1, itemA);
+                ui->resultTableWidget->setItem(i, j+2, itemD);
+            }
+        }
+        break;
+        }
 
 
 
